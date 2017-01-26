e7bb01ae60f25ea822170ad06941c19f2fdd9ca2
unknown <1403035@nchc.org.tw>
2017-01-26 09:30:16 +0800

    Decode mqtt message payload and decrypt back to plain text

--- mqtt-python/mqtt-pub.py
+++ mqtt-python/mqtt-pub.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python
 
 import paho.mqtt.publish as publish
-import time
-import random
 import subprocess
 import os, sys, logging
 from tailf import tailf
 from aes_cipher import AESCipher
 
-SEP="\x01"
+SEP = "\x01"
 
 WATCH_FILE = "/var/log/auth.log"
 
@@ -22,14 +20,15 @@ MQTT_BROKER_PORT = "1883"
 MQTT_TOPIC = "test"
 
 # logger configuration
-LOGGING_FILE = 'mqtt-client.log'
-logging.basicConfig(#filename=LOGGING_FILE,
-                    level=logging.DEBUG,
-                    format='%(asctime)s [%(levelname)s] %(filename)s_%(lineno)d  : %(message)s')
+LOGGING_FILE = 'mqtt-pub.log'
+logging.basicConfig(
+    # filename=LOGGING_FILE,
+    level=logging.DEBUG,
+    format='%(asctime)s [%(levelname)s] %(filename)s_%(lineno)d  : %(message)s')
 logger = logging.getLogger('root')
 
-def main():
 
+def main():
     logger.debug("AUGPAKE_SERVER_IP: {}".format(AUGPAKE_SERVER_IP))
     logger.debug("AUGPAKE_SERVER_PORT: {}".format(AUGPAKE_SERVER_PORT))
     logger.debug("MQTT_BROKER_IP: {}".format(MQTT_BROKER_IP))
@@ -42,8 +41,8 @@ def main():
         Key = None
 
     if Key is None:
-	logger.error("Key is None. Exit...")
-	exit()
+        logger.error("Key is None. Exit...")
+        exit()
 
     # create cipher
     cipher = AESCipher(Key)
@@ -60,18 +59,17 @@ def getKeyByAugPake(ip, port):
     """
     Return (id, key) pair
     """
-    work_dir = os.path.abspath(os.path.dirname(sys.argv[0]))+"/../augpake_src/client/"
+    work_dir = os.path.abspath(os.path.dirname(sys.argv[0])) + "/../augpake_src/client/"
     out = subprocess.check_output(["./s_client", ip, port], cwd=work_dir)
     id = out.split("-")[0].strip()
     key = out.split("-")[1].strip()
     if id:
-    	logger.info("Session ID: {}".format(id))
-    	logger.info("Session KEY: {}".format(key))
-	return id, key
+        logger.info("Session ID: {}".format(id))
+        logger.info("Session KEY: {}".format(key))
+        return id, key
     else:
         # If something wrong, return (None, None)
-	return None, None
-
+        return None, None
 
 
 if __name__ == "__main__":
--- mqtt-python/mqtt-sub.py
+++ mqtt-python/mqtt-sub.py
@@ -1,60 +1,128 @@
 #!/usr/bin/python
 
-import sys, os, time, signal
-reload(sys)
-sys.setdefaultencoding('utf-8')
+import sys, signal, logging
 import paho.mqtt.client as mqtt
 
-client = None
+from augpake import *
+from aes_cipher import AESCipher
+
+
+from thrift import Thrift
+from thrift.transport import TSocket
+from thrift.transport import TTransport
+from thrift.protocol import TBinaryProtocol
+
+# mqtt_client = None
+thrift_client = None
 mqtt_looping = False
 
+# MQTT broker configuration
+MQTT_BROKER_IP = "localhost"
+MQTT_BROKER_PORT = "1883"
 TOPIC_ROOT = "test"
+
+THRIFT_SERVER_IP = "localhost"
+THRIFT_SERVER_PORT = 9090
+
+SEP = "\x01"
+
+SESSION_KEY_CACHE = {}      # <id, key> pair
+SESSION_CIPHER_CACHE = {}   # <id, cipher> pair
+
+# logger configuration
+LOGGING_FILE = 'mqtt-sub.log'
+logging.basicConfig(
+    # filename=LOGGING_FILE,
+    level=logging.DEBUG,
+    format='%(asctime)s [%(levelname)s] %(filename)s_%(lineno)d  : %(message)s')
+logger = logging.getLogger('root')
+
 
 def on_connect(mq, userdata, rc, _):
     # subscribe when connected.
     mq.subscribe(TOPIC_ROOT + '/#')
 
+
 def on_message(mq, userdata, msg):
-    print "topic: %s" % msg.topic
-    print "payload: %s" % msg.payload
-    print "qos: %d" % msg.qos
+    logger.debug("payload: %s" % msg.payload)
 
-def mqtt_client_thread():
-    global client, mqtt_looping
-    client_id = "" # If broker asks client ID.
-    client = mqtt.Client(client_id=client_id)
+    text = DecodePayload(msg.payload)
+    if text is not None:
+        syslog_output(text)
+    else:
+        logger.error("Decode payload {%s} fail." % msg.payload)
 
-    # If broker asks user/password.
-    user = ""
-    password = ""
-    client.username_pw_set(user, password)
 
-    client.on_connect = on_connect
-    client.on_message = on_message
+def DecodePayload(payload):
+    global SESSION_KEY_CACHE
+    global SESSION_CIPHER_CACHE
 
     try:
-        client.connect("localhost")
+        ID = payload.decode("utf-8").split(SEP)[0]
+        msg = payload.decode("utf-8").split(SEP)[1]
+    except IndexError as e:
+        logger.error(e)
+        return None
+
+    if ID not in SESSION_KEY_CACHE:
+        logger.info("Session key for ID %s is not cached, query by thrift server." % ID)
+        SESSION_KEY_CACHE[ID] = thrift_client.SearchKey(ID)
+
+    if ID not in SESSION_CIPHER_CACHE:
+        logger.info("Cipher for ID %s is not cached, create one." % ID)
+        SESSION_CIPHER_CACHE[ID] = AESCipher(SESSION_KEY_CACHE[ID])
+
+    return SESSION_CIPHER_CACHE[ID].decrypt(msg)
+
+
+#TODO
+def syslog_output(msg):
+    logger.debug("Received Plain text: " % msg)
+
+
+
+def thrift_client_init():
+
+    # client = None
+    # transport = None
+    try:
+        transport = TSocket.TSocket(THRIFT_SERVER_IP, THRIFT_SERVER_PORT)
+        transport = TTransport.TBufferedTransport(transport)
+        protocol = TBinaryProtocol.TBinaryProtocol(transport)
+        client = AugPAKE.Client(protocol)
+        transport.open()
+
+    except Thrift.TException as ex:
+        logger.error("%s" % ex.message)
+        sys.exit(0)
+
+    return client, transport
+
+
+def mqtt_client_thread():
+    global mqtt_looping, thrift_client
+
+    thrift_client, thrift_connection = thrift_client_init()
+    mqtt_client = mqtt.Client()
+    mqtt_client.on_connect = on_connect
+    mqtt_client.on_message = on_message
+
+    try:
+        mqtt_client.connect(MQTT_BROKER_IP)
     except:
-        print "MQTT Broker is not online. Connect later."
+        logger.error("MQTT Broker is not online. Connect later.")
+        sys.exit(0)
 
     mqtt_looping = True
-    print "Looping..."
+    logger.info("Mqtt client thread listen on %s" % (TOPIC_ROOT))
 
-    #mqtt_loop.loop_forever()
-    cnt = 0
     while mqtt_looping:
-        client.loop()
+        mqtt_client.loop()
 
-        cnt += 1
-        if cnt > 20:
-            try:
-                client.reconnect() # to avoid 'Broken pipe' error.
-            except:
-                time.sleep(1)
-            cnt = 0
+    logger.info("mqtt client thread quit")
+    mqtt_client.disconnect()
+    thrift_connection.close()
 
-    print "quit mqtt thread"
-    client.disconnect()
 
 def stop_all(*args):
     global mqtt_looping
@@ -62,10 +130,6 @@ def stop_all(*args):
 
 if __name__ == '__main__':
     signal.signal(signal.SIGTERM, stop_all)
-    signal.signal(signal.SIGQUIT, stop_all)
     signal.signal(signal.SIGINT,  stop_all)  # Ctrl-C
-
     mqtt_client_thread()
-
-    print "exit program"
     sys.exit(0)

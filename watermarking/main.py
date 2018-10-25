4d02ab2f415eaa4bce8beca466bcb3bfae6feb4d
Jimmy Chuang <ogre0403@gmail.com>
2018-10-25 11:26:06 +0800

    recoover watermarking image

--- watermarking/main.py
+++ watermarking/main.py
@@ -1,37 +1,22 @@
-import watermark
 import reversible
-
-import monotonic
 
 
 def main():
-    # for b in range(256):
-    #     aRow = []
-    #     for q in range(256):
-    #         aRow.append(reversible.F_inverse(b, q))
-    #     print(str(aRow) + ",")
+    p = (4, 4, 4)
+    a = (3, 3, 3)
+    b = (1, 1, 1)
 
-    # print monotonic.F_inv_Table[3][5]
+    print "origin pixel\t: %s" % (p,)
+    q = reversible.F_inverse(b, reversible.F(a, p))
+    print "watermark pixel\t: %s" % (q,)
+    p_prime = reversible.F_inverse(a, reversible.F(b, q))
+    print "recovered pixel\t: %s" % (p_prime,)
 
-    # p = (4, 4, 4)
-    # a = (3, 3, 3)
-    # b = (1, 1, 1)
-    #
-    # q = reversible.F_inverse_rgb(b, reversible.F_rgb(a, p))
-    # print q
-    # pp = reversible.F_inverse_rgb(a, reversible.F_rgb(b, q))
-    # print pp
+    estimate_A, watermarking_img = reversible.addWatermark("./aa.jpg", "./logo.png")
+    watermarking_img.show()
 
-    # print F(224, 227)
-    # print F_inverse(30, F(224, 227))
-    # print F_inverse(224, F(30, 33))
-    # watermark_pixel("./logo.png")
-    # watermark_pixel("./water-2.jpg")
-    # watermark.watermark_with_transparency("./aa.jpg", "./ww.jpg", './watermark.png', position=(0, 0))  # functio
-    # watermark.watermark_with_transparency("./watermark.png", "./ww.jpg", './logo.png', position=(0, 0))  # functio
-    A, B = reversible.addWatermark("./aa.jpg", "./logo.png")
-
-    print A[423][477]
+    recovered_img = reversible.removeWatermark(watermarking_img, estimate_A, "./logo.png")
+    recovered_img.show()
 
 
 if __name__ == "__main__":

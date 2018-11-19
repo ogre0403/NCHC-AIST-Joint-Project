035e87785110af536a444ace6ced5e2a5a7a341b
Jimmy Chuang <ogre0403@gmail.com>
2018-11-19 16:30:18 +0800

    add watermark by block

--- watermarking/reversible.py
+++ watermarking/reversible.py
@@ -22,37 +22,47 @@ def estimate_a(p1, p2, p3):
     return p
 
 
-def addWatermark(base, watermark):
-    base_image = Image.open(base)
-    base_rgb = base_image.convert('RGB')
-    base_pixel = base_image.load()
-    width, height = base_image.size
+def addWatermark(origin, watermark, blk_width=0, blk_height=0):
+    origin_image = Image.open(origin)
+    origin_pixel = origin_image.load()
+    width, height = origin_image.size
 
-    watermark = Image.open(watermark).resize(base_image.size)
+    if blk_height != 0 and blk_width != 0:
+        width = blk_width
+        height = blk_height
+
+    result_image = Image.new('RGB', (width, height))
+    result_pixel = result_image.load()
+
+    # todo: add watermark block by block
+    addWatermarkBlock((0, 0), (width - 1, height - 1), watermark, origin_pixel, result_pixel)
+    return result_image
+
+
+def addWatermarkBlock(start_wh, end_wh, watermark, origin_pixel, result_pixel):
+    width = end_wh[0] - start_wh[0] + 1
+    height = end_wh[1] - start_wh[1] + 1
+
+    watermark = Image.open(watermark).resize((width, height))
     watermark_rgb = watermark.convert('RGB')
 
-    result = Image.new('RGB', base_image.size)
-    pixels = result.load()
-
     for w in range(width):
-        pixels[w, height - 1] = base_pixel[w, height - 1]
+        result_pixel[w, end_wh[1]] = origin_pixel[w, end_wh[1]]
 
     for h in range(height):
-        pixels[width - 1, h] = base_pixel[width - 1, h]
+        result_pixel[end_wh[0], h] = origin_pixel[end_wh[0], h]
 
-    for h in range(0, height - 1):
-        for w in range(0, width - 1):
+    for h in range(start_wh[1], height - 1):
+        for w in range(start_wh[0], width - 1):
             if watermark_rgb.getpixel((w, h)) != (255, 255, 255):
-                a = estimate_a(base_rgb.getpixel((w + 1, h)),
-                               base_rgb.getpixel((w, h + 1)),
-                               base_rgb.getpixel((w + 1, h + 1)))
+                a = estimate_a(origin_pixel[w + 1, h],
+                               origin_pixel[w, h + 1],
+                               origin_pixel[w + 1, h + 1])
 
                 b = watermark_rgb.getpixel((w, h))
-                pixels[w, h] = F_inverse(b, F(a, base_rgb.getpixel((w, h))))
+                result_pixel[w, h] = F_inverse(b, F(a, origin_pixel[w, h]))
             else:
-                pixels[w, h] = base_pixel[w, h]
-
-    return result
+                result_pixel[w, h] = origin_pixel[w, h]
 
 
 def removeWatermark(coverImg, watermark):
@@ -84,3 +94,7 @@ def removeWatermark(coverImg, watermark):
                 pixels[w, h] = base_pixel[w, h]
 
     return result
+
+
+def removeWatermarkBlock(coverImg, watermark):
+    pass

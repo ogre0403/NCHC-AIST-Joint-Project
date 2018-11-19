ac00f0ddee2fb66c7e79d538d05e2ad3522aae9e
Jimmy Chuang <ogre0403@gmail.com>
2018-11-19 16:53:30 +0800

    remove watermark by block

--- watermarking/reversible.py
+++ watermarking/reversible.py
@@ -52,8 +52,8 @@ def addWatermarkBlock(start_wh, end_wh, watermark, origin_pixel, result_pixel):
     for h in range(height):
         result_pixel[end_wh[0], h] = origin_pixel[end_wh[0], h]
 
-    for h in range(start_wh[1], height - 1):
-        for w in range(start_wh[0], width - 1):
+    for h in range(start_wh[1], end_wh[1]):
+        for w in range(start_wh[0], end_wh[0]):
             if watermark_rgb.getpixel((w, h)) != (255, 255, 255):
                 a = estimate_a(origin_pixel[w + 1, h],
                                origin_pixel[w, h + 1],
@@ -65,36 +65,44 @@ def addWatermarkBlock(start_wh, end_wh, watermark, origin_pixel, result_pixel):
                 result_pixel[w, h] = origin_pixel[w, h]
 
 
-def removeWatermark(coverImg, watermark):
-    width, height = coverImg.size
+def removeWatermark(masked_image, watermark, blk_width=0, blk_height=0):
+    width, height = masked_image.size
 
-    watermark = Image.open(watermark).resize(coverImg.size)
+    if blk_height != 0 and blk_width != 0:
+        width = blk_width
+        height = blk_height
+
+    result_image = Image.new('RGB', (width, height))
+    result_pixel = result_image.load()
+
+    masked_pixel = masked_image.load()
+
+    # todo: remove watermark block by block
+    removeWatermarkBlock((0, 0), (width - 1, height - 1), watermark, masked_pixel, result_pixel)
+
+    return result_image
+
+
+def removeWatermarkBlock(start_wh, end_wh, watermark_file, masked_pixel, result_pixel):
+    width = end_wh[0] - start_wh[0] + 1
+    height = end_wh[1] - start_wh[1] + 1
+
+    watermark = Image.open(watermark_file).resize((width, height))
     watermark_rgb = watermark.convert('RGB')
 
-    result = Image.new('RGB', (width, height))
-    pixels = result.load()
-
-    base_pixel = coverImg.load()
-
     for w in range(width):
-        pixels[w, height - 1] = base_pixel[w, height - 1]
+        result_pixel[w, end_wh[1]] = masked_pixel[w, end_wh[1]]
 
     for h in range(height):
-        pixels[width - 1, h] = base_pixel[width - 1, h]
+        result_pixel[end_wh[0], h] = masked_pixel[end_wh[0], h]
 
-    for h in range(height - 2, -1, -1):
-        for w in range(width - 2, -1, -1):
+    for h in range(end_wh[1], start_wh[1], -1):
+        for w in range(end_wh[0], start_wh[0], -1):
             if watermark_rgb.getpixel((w, h)) != (255, 255, 255):
-                a = estimate_a(pixels[w + 1, h],
-                               pixels[w, h + 1],
-                               pixels[w + 1, h + 1])
+                a = estimate_a(result_pixel[w + 1, h],
+                               result_pixel[w, h + 1],
+                               result_pixel[w + 1, h + 1])
                 b = watermark_rgb.getpixel((w, h))
-                pixels[w, h] = F_inverse(a, F(b, base_pixel[w, h]))
+                result_pixel[w, h] = F_inverse(a, F(b, masked_pixel[w, h]))
             else:
-                pixels[w, h] = base_pixel[w, h]
-
-    return result
-
-
-def removeWatermarkBlock(coverImg, watermark):
-    pass
+                result_pixel[w, h] = masked_pixel[w, h]
